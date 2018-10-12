0eb729eb17940757a850a40d48408427bc2a127b
91Snake <agarciasobrado@gmail.com>
2018-10-12 16:17:57 -0300

    User

--- config/initializers/afip.rb
+++ config/initializers/afip.rb
@@ -3,6 +3,6 @@ Afip.setup do |config|
 	config.cert = "#{Rails.root}/app/certs/sanantonio.cert"
 	config.environment = :test
 	config.cuit = "30709038148"
-	config.openssl_bin = "/usr/local/Cellar/openssl/1.0.2o_2/bin/openssl"
+	config.openssl_bin = "/usr/bin/openssl"
 	config.service_url = "https://fwshomo.afip.gov.ar/wsctg/services/CTGService_v3.0?wsdl"
 end
