b5fc81ac1ee3dd770f8885bcb0c6e3d11c2a159b
91Snake <agarciasobrado@gmail.com>
2018-10-16 16:02:23 -0300

    asd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
- before_action :authenticate_user!
+ before_action :configure_permitted_parameters, :authenticate_user!, if: :devise_controller?
 
 	def get_localities
 		render json: ::Afip::CTG.new().consultar_localidades(params[:city_id])
 	end
+
+  protected
+
+  def configure_permitted_parameters
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    # devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :dni, :birthday, :address, :phone, :mobile_phone, :province, :city, :province, :postal_code])
+  end
+
 end
