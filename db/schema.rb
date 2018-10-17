49763a22dde0c5c11faf717e94cb8124d9d2bf89
Facundo A. Díaz Martínez <facundo_diaz_martinez@hotmail.com>
2018-10-17 12:41:24 -0300

    foo

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_133806) do
+ActiveRecord::Schema.define(version: 2018_10_17_153501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "phone"
+    t.string "mobile_phone"
+    t.string "email"
+    t.string "address"
+    t.string "document_type", default: "D.N.I.", null: false
+    t.string "document_number", null: false
+    t.string "birthday"
+    t.boolean "active", default: true, null: false
+    t.string "iva_cond", default: "Responsable Monotributo", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "email", null: false
