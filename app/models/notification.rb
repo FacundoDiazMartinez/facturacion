8568fb29f5814b9a0cfb216f134b1cc9ac07206b
Ing. Facundo A. Díaz Martínez <facundodiazmartinez14@gmail.com>
2018-12-21 11:04:53 -0300

    foo

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -11,20 +11,21 @@ class Notification < ApplicationRecord
 				"/facturacion/notifications/#{receiver_id}",
 				"$('.notifications').html('#{receiver.count_notifications}');
 				 document.getElementById('notification_sound').play();
-				 document.title = '(#{receiver.count_notifications}) - Desideral.com'"
+				 document.title = '(#{receiver.count_notifications}) - LiteCode.com.ar'"
 			)
 		end
 
 		def self.create_from_payment payment
 		    payment.delayed_job.destroy unless payment.delayed_job.nil?
-
-		    Notification.delay(run_at: payment.payment_date, payment_id: payment.id).create(
-		        title:        "Debe cobrar un pago.",
-		        body:         "Se generó una alerta por el cobro de un pago que debería ser acreditado hoy.",
-		        link:         "/invoices/#{payment.invoice_id}",
-		        sender_id:    payment.invoice.user_id,
-		        receiver_id:  User.last.id
-		    )
+		    if payment.payment_date > Date.today
+			    Notification.delay(run_at: payment.payment_date, payment_id: payment.id).create(
+			        title:        "Debe cobrar un pago.",
+			        body:         "Se generó una alerta por el cobro de un pago que debería ser acreditado hoy. El tipo de pago es: #{payment.payment_name}",
+			        link:         "/invoices/#{payment.invoice_id}.pdf",
+			        sender_id:    payment.invoice.user_id,
+			        receiver_id:  User.last.id
+			    )
+			end
 		end
 
 		def self.create_for_failed_import invalid, user
