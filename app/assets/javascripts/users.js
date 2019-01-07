df033de1d1599063719b9a9416a425dabda2fdfb
diegoabud92 <diegoabud92@hotmail.com>
2019-01-07 15:08:12 -0300

    Attr

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,16 +4,16 @@ function changeInformation(action){
       $("#basic_info").attr('style', 'display: block');
       $("#account").attr('style', 'display: none');
       //$("#personal_i").removeClass("active")
-      $("#account_i").find('a').removeClass("active")
-      $("#personal_i").find('a').addClass("active")
+      $("#account_i").find('a').removeClass("active");
+      $("#personal_i").find('a').addClass("active");
     }
     if (action == 'user')
     {
       $("#basic_info").attr('style', 'display: none');
       $("#account").attr('style', 'display: block');
-      $("#personal_i").find('a').removeClass("active")
+      $("#personal_i").find('a').removeClass("active");
       //$("#account_i").removeClass("active")
-      $("#account_i").find('a').addClass("active")
+      $("#account_i").find('a').addClass("active");
     }
   }
 
@@ -21,9 +21,13 @@ function displayForm(type){
   $(".card-options").hide();  
   $("#form").show("slow");
   if (type == "manager") {
-    $("#company_code").hide().removeAttr("required");
+    $("#company_code").hide();
+    $("#code").removeAttr("required");
+    $("#user_company_id").removeAttr("required");
   }else{
-     $("#company_code").show().attr("required", "true");
+     $("#company_code").show();
+     $("#code").attr("required", "required");
+     $("#user_company_id").attr("required", "required");
   }
 }
 
