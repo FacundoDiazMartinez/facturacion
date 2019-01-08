4f3ed21d53a04b61105cb59c40d5f2639b910d6b
Ing. Facundo A. Díaz Martínez <facundodiazmartinez14@gmail.com>
2019-01-08 11:09:50 -0300

    foo

--- db/migrate/20190107233831_change_number_to_purchase_orders.rb
+++ db/migrate/20190107233831_change_number_to_purchase_orders.rb
@@ -1,5 +1,5 @@
 class ChangeNumberToPurchaseOrders < ActiveRecord::Migration[5.2]
   def change
-  	change_column :purchase_orders, :number, :string, null: false
+  	#change_column :purchase_orders, :number, :string, null: false
   end
 end
