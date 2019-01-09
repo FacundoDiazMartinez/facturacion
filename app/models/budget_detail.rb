b1a16e344592898d9a178395baa4a49c1d0e37fe
Ing. Facundo A. Díaz Martínez <facundodiazmartinez14@gmail.com>
2019-01-09 14:44:15 -0300

    foo

--- app/models/budget_detail.rb
+++ app/models/budget_detail.rb
@@ -3,10 +3,21 @@ class BudgetDetail < ApplicationRecord
   belongs_to :budget
   belongs_to :depot, optional: true
 
+  after_save :adjust_reserved_stock, if: :saved_change_to_quantity?
+  
   #ATRIBUTOS
   	def product_code
   		product.nil? ? "" : product.code
   	end
   #ATRIBUTOS
+
+   #PROCEOS
+	   	def adjust_reserved_stock
+	      	if budget.reserv_stock
+	        	dif = quantity - quantity_was
+	        	product.reserve_stock(quantity: self.quantity, depot_id: depot_id)
+	      	end
+	    end
+   #PROCEOS
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2019_01_08_200034) do
     t.bigint "purchase_order_id"
     t.bigint "user_id"
     t.bigint "depot_id"
-    t.integer "number", null: false
+    t.string "number", null: false
     t.boolean "active", default: true, null: false
     t.string "state", default: "Pendiente", null: false
     t.datetime "created_at", null: false
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 2019_01_08_200034) do
   end
 
   create_table "budgets", force: :cascade do |t|
-    t.date "date", default: -> { "('now'::text)::date" }, null: false
+    t.date "date", default: -> { "CURRENT_DATE" }, null: false
     t.string "state", default: "Pendiente", null: false
     t.date "expiration_date"
     t.string "number", null: false
@@ -142,6 +142,7 @@ ActiveRecord::Schema.define(version: 2019_01_08_200034) do
     t.boolean "valid_for_account", default: true, null: false
     t.bigint "user_id"
     t.index ["company_id"], name: "index_clients_on_company_id"
+    t.index ["user_id"], name: "index_clients_on_user_id"
   end
 
   create_table "commissioners", force: :cascade do |t|
@@ -522,7 +523,7 @@ ActiveRecord::Schema.define(version: 2019_01_08_200034) do
   end
 
   create_table "purchase_orders", force: :cascade do |t|
-    t.integer "number", null: false
+    t.string "number", null: false
     t.string "state", default: "Pendiente de aprobación", null: false
     t.bigint "supplier_id"
     t.text "observation"
@@ -698,6 +699,7 @@ ActiveRecord::Schema.define(version: 2019_01_08_200034) do
   add_foreign_key "budgets", "users"
   add_foreign_key "client_contacts", "clients"
   add_foreign_key "clients", "companies"
+  add_foreign_key "clients", "users"
   add_foreign_key "commissioners", "invoice_details"
   add_foreign_key "commissioners", "users"
   add_foreign_key "credit_card_payments", "credit_cards"
