c5758ef7e25a377e090473bf954b8ffb6d9ee3ba
91Snake <agarciasobrado@gmail.com>
2019-01-31 10:35:21 -0300

    fixes

--- app/assets/javascripts/receipts.js
+++ app/assets/javascripts/receipts.js
@@ -1,4 +1,9 @@
 $(document).on('railsAutocomplete.select', '.receipt_associated-invoice-autocomplete_field', function(event, data){
+  params = {
+		associated_invoice_id: data.item.id,
+	}
+	form = $(this).parents('form:first');
+	$.get(form.attr("action")+'/set_associated_invoice', params, null, "script");
 	if ($("#invoice_comp_number").val() != "") {
 		$('#editReceiptClient').attr("data-toggle", "");
 		$('#editReceiptClient').tooltip({title: "No es posible editar cliente mientras exista una factura vinculada."});
