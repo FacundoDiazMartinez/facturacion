519d0900de18360696dd860b733ffa2ef3462a25
diegoabud92 <diegoabud92@hotmail.com>
2019-02-19 12:16:47 -0300

    pull

--- spec/factories/product.rb
+++ spec/factories/product.rb
@@ -2,10 +2,11 @@ FactoryBot.define do
   factory :product do
     association :product_category
     association :company
-    association :created_by
+    association :created_by, factory: :user
+    association :updated_by, factory: :user
   	sequence(:code) { |n| "#{n}CODE" }
   	active {true}
-  	cost_price {}
+  	cost_price {220}
     price {22}
     percentage {"Infinity"}
   end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Product, type: :model do
   it { should validate_presence_of(:active) }
   it { should validate_presence_of(:company) }
   it { should validate_presence_of(:net_price) }
+  it { should validate_presence_of(:cost_price) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:iva_aliquot) }
   it { should validate_presence_of(:created_by) }
