ca68e2d46b846072b18d111df055f67810d3300f
Ing. Díaz Martínez, Facundo Adrián <facundodiazmartinez14@gmail.com>
2019-03-26 10:59:06 -0300

    Fix flow in daily cash and fix credit note with duplicated details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_144916) do
+ActiveRecord::Schema.define(version: 2019_03_26_133627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -425,6 +425,7 @@ ActiveRecord::Schema.define(version: 2019_03_25_144916) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "depot_id"
+    t.bigint "associated_detail"
     t.index ["depot_id"], name: "index_invoice_details_on_depot_id"
     t.index ["invoice_id"], name: "index_invoice_details_on_invoice_id"
     t.index ["product_id"], name: "index_invoice_details_on_product_id"
