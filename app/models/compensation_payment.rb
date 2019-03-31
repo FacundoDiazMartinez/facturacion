709ec6ffc79255d8e2d8f08b776caef8807ef38f
Andres Garcia <agarciasobrado@gmail.com>
2019-03-31 18:06:08 -0300

    fixes on compensation payment and cr card

--- app/models/compensation_payment.rb
+++ app/models/compensation_payment.rb
@@ -1,5 +1,7 @@
 class CompensationPayment < ApplicationRecord
   include Subpayment
   	belongs_to :payment
-    belongs_to :client
+    belongs_to :client, optional: true
+
+    validates_presence_of :client_id, message: "El pago por compensación debe estar asociado a un cliente."
 end
