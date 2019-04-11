2935b9c38b9c9670aab73d9135f2ded31941ae02
91Snake <agarciasobrado@gmail.com>
2019-04-11 16:00:36 -0300

    fix on daily cash movements

--- app/models/expense_payment.rb
+++ app/models/expense_payment.rb
@@ -5,6 +5,7 @@ class ExpensePayment < Payment
 
 	before_validation :set_flow
 	before_save :check_company_id
+	before_destroy :touch_purchase_order
 
 	def self.default_scope
     	where(flow: "expense", active: true)
@@ -16,6 +17,10 @@ class ExpensePayment < Payment
 
 	def check_company_id
 		self.company_id = purchase_order.company_id
+	end
+
+	def touch_purchase_order
+		self.purchase_order.set_total_pay
 	end
 
 end
