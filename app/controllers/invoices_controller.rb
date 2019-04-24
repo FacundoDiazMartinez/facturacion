b917aa10b4bda21ee46c3b8516e56c249d30726b
diegoabud92 <diegoabud92@hotmail.com>
2019-04-24 15:08:57 -0300

    fix pdf

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -28,8 +28,7 @@ class InvoicesController < ApplicationController
         render pdf: "Factura_#{@invoice.comp_number}_#{@invoice.client.name}",
           layout: 'pdf.html',
           template: 'invoices/show',
-          #si dejamos el zoom en 1, el numero de página se pasa de página
-          #zoom: 0.9,
+          #zoom: 1, si en local se ve mal, poner en 3.5 solo para local
           viewport_size: '1280x1024',
           page_size: 'A4',
           encoding:"UTF-8"
