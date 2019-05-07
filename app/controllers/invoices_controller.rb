b6a856d2911412a28fb985048aa3aa12eca56da8
diegoabud92 <diegoabud92@hotmail.com>
2019-05-07 12:59:05 -0300

    push a hk2

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -28,7 +28,7 @@ class InvoicesController < ApplicationController
         render pdf: "Factura_#{@invoice.comp_number}_#{@invoice.client.name}",
           layout: 'pdf.html',
           template: 'invoices/show',
-          zoom: 3.5,
+          #zoom: 3.4,
           #si en local se ve mal, poner en 3.5 solo para local
           viewport_size: '1280x1024',
           page_size: 'A4',
