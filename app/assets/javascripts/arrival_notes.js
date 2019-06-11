d60a94699c1e1d76482967c0558b9f4a3a5deac9
91Snake <agarciasobrado@gmail.com>
2019-06-11 12:46:36 -0300

    fix purchase order - arrival note

--- app/assets/javascripts/arrival_notes.js
+++ app/assets/javascripts/arrival_notes.js
@@ -39,7 +39,7 @@ $( document ).ready(function() {
 
 $(document).on('railsAutocomplete.select', '.arrival_note-purchase_order-autocomplete_field', function(event, data){
 	params = {
-		purchase_order_id: data.item.id, 
+		purchase_order_id: data.item.id,
 		depot_id: $("#arrival_note_depot_id").val(),
 		state: $("#arrival_note_state").val(),
 		number: $("#arrival_note_number").val()
