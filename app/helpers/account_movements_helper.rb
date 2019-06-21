945169ecd541244eb0fbac85d36590b6ae5ab365
diegoabud92 <diegoabud92@hotmail.com>
2019-06-21 05:15:04 -0300

    push origin

--- app/helpers/account_movements_helper.rb
+++ app/helpers/account_movements_helper.rb
@@ -7,7 +7,7 @@ module AccountMovementsHelper
 				if account_movement.cbte_tipo == "Devolución"
 					"#{account_movement.cbte_tipo.split().map{|w| w.first unless w.first != w.first.upcase}.join()} - #{account_movement.receipt.sale_point.name} -  #{account_movement.receipt.number}"
 				else  # Recibo X
-					link_to "#{account_movement.cbte_tipo[-1,1]} - #{account_movement.receipt.nil? ? "" : account_movement.receipt.sale_point.name} - #{account_movement.receipt.nil? ? "" : account_movement.receipt.number}", edit_receipt_path(account_movement.receipt_id)
+					link_to "#{(account_movement.cbte_tipo=="00"? "X" : account_movement.cbte_tipo[-1,1])} - #{account_movement.receipt.nil? ? "" : account_movement.receipt.sale_point.name} - #{account_movement.receipt.nil? ? "" : account_movement.receipt.number}", edit_receipt_path(account_movement.receipt_id)
 	       	# link_to  "#{account_movement.cbte_tipo.split().map{|w| w.first unless w.first != w.first.upcase}.join()} - #{account_movement.receipt.number}", edit_receipt_path(account_movement.receipt_id)
 				end
 	    end
