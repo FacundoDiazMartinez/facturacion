1adfd5683478ba0c015352679bb92d8034c8b1ef
diegoabud92 <diegoabud92@hotmail.com>
2019-07-02 09:32:14 -0300

    send_to_client_email

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -8,6 +8,11 @@ class InvoiceMailer < ApplicationMailer
 	    @barcode_path = barcode_path
 		attachments['factura.pdf'] = render_to_string(pdf: "factura.pdf", template: 'invoices/show.pdf.erb', layout: 'pdf.html')
 		mail to: email, subject: "Factura Nº #{invoice.full_number} - #{invoice.company.name}"
+		# unless @invoice.nil?
+	 #      	request_attach = PurchaseRequestPdf.new(@invoice, @budget_request.id)
+	 #      	attachments["solicitud-#{@budget_request.supplier_name}-#{Date.today}.pdf"] = { :mime_type => 'application/pdf', :content => request_attach.render }
+	 #      	mail to: @email, subject: "Solicitud de presupuesto"
+	 #    end
 	end
 
 end
