e48d7ecc51fa011ba1f278c3e7f7ed9760f58a7a
diegoabud92 <diegoabud92@hotmail.com>
2019-07-26 12:56:12 -0300

    sales_per_product_by_year axslx

--- app/presenters/product_presenter.rb
+++ app/presenters/product_presenter.rb
@@ -9,8 +9,8 @@ class ProductPresenter
 	  invoices = 0
 	  product = @product.id
 	  company = @product.company_id
-	  invoices = InvoiceDetail.joins(:invoice).where(product_id: product).where(invoices: {company_id: company, state: "Confirmado", cbte_tipo: ("01" || "06" || "11"), cbte_fch: (Date.today.at_beginning_of_year-1).to_s .. (Date.today.at_end_of_year-1).to_s}).sum(:quantity)
-	  credit_notes = InvoiceDetail.joins(:invoice).where(product_id: product).where(invoices: {company_id: company, state: "Confirmado", cbte_tipo: ("03" || "08" || "13"), cbte_fch: (Date.today.at_beginning_of_year-1).to_s .. (Date.today.at_end_of_year-1).to_s}).sum(:quantity)
+	  invoices = InvoiceDetail.joins(:invoice).where(product_id: product).where(invoices: {company_id: company, cbte_tipo: ['01','06','11'], cbte_fch: (Date.today.at_beginning_of_year-1).to_s .. (Date.today.at_end_of_year-1).to_s}).sum(:quantity)
+	  credit_notes = InvoiceDetail.joins(:invoice).where(product_id: product).where(invoices: {company_id: company, cbte_tipo: ['03','08','13'], cbte_fch: (Date.today.at_beginning_of_year-1).to_s .. (Date.today.at_end_of_year-1).to_s}).sum(:quantity)
 	  return invoices - credit_notes
  	end
 
@@ -18,8 +18,8 @@ class ProductPresenter
 	  invoices = 0
 	  product = @product.id
 	  company = @product.company_id
-	  invoices = InvoiceDetail.joins(:invoice).where(product_id: product).where(invoices: {company_id: company, state: "Confirmado", cbte_tipo: ("01" || "06" || "11"), cbte_fch: (Date.today.at_beginning_of_year).to_s .. (Date.today.at_end_of_year).to_s}).sum(:quantity)
-	  credit_notes = InvoiceDetail.joins(:invoice).where(product_id: product).where(invoices: {company_id: company, state: "Confirmado", cbte_tipo: ("03" || "08" || "13"), cbte_fch: (Date.today.at_beginning_of_year).to_s .. (Date.today.at_end_of_year).to_s}).sum(:quantity)
+	  invoices = InvoiceDetail.joins(:invoice).where(product_id: product).where(invoices: {company_id: company, cbte_tipo: ['01','06','11'], cbte_fch: (Date.today.at_beginning_of_year).to_s .. (Date.today.at_end_of_year).to_s}).sum(:quantity)
+	  credit_notes = InvoiceDetail.joins(:invoice).where(product_id: product).where(invoices: {company_id: company, cbte_tipo: ['03','08','13'], cbte_fch: (Date.today.at_beginning_of_year).to_s .. (Date.today.at_end_of_year).to_s}).sum(:quantity)
 	  return invoices - credit_notes
  	end
 end
