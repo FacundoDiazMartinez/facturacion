87cbb72087d1489f526d8c18dd7f1040ddb9f256
Ariel <arielg2094@gmail.com>
2019-07-26 12:10:46 -0300

    fix invoices

--- app/models/product.rb
+++ app/models/product.rb
@@ -67,10 +67,10 @@ class Product < ApplicationRecord
 
   validates :code,
     presence: { message: "Debe ingresar un código en el producto." },
-    uniqueness: { scope: [:company_id, :active, :tipo], message: "Ya existe un producto con el mismo identificador."}
+    uniqueness: { scope: [:company_id, :active, :tipo], message: "Ya existe un producto con el mismo identificador.", if: :active }
   validates_uniqueness_of :name,
     presence: { message: "El nombre del producto no puede estar en blanco." },
-    uniqueness: { scope: [:company_id, :active], message: "Ya existe un producto con el mismo nombre."}
+    uniqueness: { scope: [:company_id, :active], message: "Ya existe un producto con el mismo nombre.", if: :active }
 	validates_presence_of :price,
     presence: { message: "Debe ingresar el precio del producto." },
     numericality: { greater_than: 0, message: "El precio debe ser mayor a 0." }
