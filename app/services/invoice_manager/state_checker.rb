04e3608505c24c598e08be489d49294cffdcfeb7
diegoabud92 <diegoabud92@hotmail.com>
2019-08-06 14:01:04 -0300

    foo

--- app/services/invoice_manager/state_checker.rb
+++ app/services/invoice_manager/state_checker.rb
@@ -1,31 +1,31 @@
-module InvoiceManager
-	class	StateChecker
-    def initialize (invoice)
-			@associated_invoice = invoice.invoice
-      @invoice = invoice
-		end
-
-    def call
-			#check_state
-		end
-
-    def check_state
-      anulada_totalmente = true
-      pp invoice_quantity = 0
-      pp cn_detail_quantity  = 0
-      @associated_invoice.invoice_details.each do |invoice_detail|
-        invoice_quantity += invoice_detail.quantity
-        @associated_invoice.credit_notes.each do |credit_note|
-          credit_note.invoice_details.each do |cn_detail|
-            cn_detail_quantity += cn_detail.quantity
-          end
-        end
-      end
-    end
-    if invoice_quantity == cn_detail_quantity
-      @associated_invoice.update_columns(state: "Anulado")
-    else
-      @associated_invoice.update_columns(state: "Anulado parcialmente")
-    end
-	end
-end
+# module InvoiceManager
+# 	class	StateChecker
+#     def initialize (invoice)
+# 			@associated_invoice = invoice.invoice
+#       @invoice = invoice
+# 		end
+#
+#     def call
+# 			#check_state
+# 		end
+#
+#     def check_state
+#       anulada_totalmente = true
+#       pp invoice_quantity = 0
+#       pp cn_detail_quantity  = 0
+#       @associated_invoice.invoice_details.each do |invoice_detail|
+#         invoice_quantity += invoice_detail.quantity
+#         @associated_invoice.credit_notes.each do |credit_note|
+#           credit_note.invoice_details.each do |cn_detail|
+#             cn_detail_quantity += cn_detail.quantity
+#           end
+#         end
+#       end
+#     end
+#     if invoice_quantity == cn_detail_quantity
+#       @associated_invoice.update_columns(state: "Anulado")
+#     else
+#       @associated_invoice.update_columns(state: "Anulado parcialmente")
+#     end
+# 	end
+# end
