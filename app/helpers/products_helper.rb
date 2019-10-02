e77798c8a61b52f40817b0acebf6aa63e16b3025
diegoabud92 <diegoabud92@hotmail.com>
2019-10-02 15:55:02 -0300

    Service del importador

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,22 +1,21 @@
 module ProductsHelper
 
 	def category_with_link product
-		if product.supplier_id.blank? 
+		if product.supplier_id.blank?
 			product.category_name
 		else
 			if product.product_category.present?
 				link_to product.category_name, product_category_path(product.product_category_id)
 			else
-				
 			end
 		end
 	end
 
 	def supplier_with_link product
-		if product.supplier_id.blank? 
-			product.supplier_name
-		else
+		if product.supplier_id.blank?
 			link_to product.category_name, edit_supplier_path(product.supplier_id)
+		else
+			product.supplier_name
 		end
 	end
 
