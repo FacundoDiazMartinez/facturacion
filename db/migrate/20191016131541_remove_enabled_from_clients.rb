c3cf25d3e691b185b1b7bd8e089b0b2b357b928d
diegoabud92 <diegoabud92@hotmail.com>
2019-10-16 10:26:43 -0300

    fix enabled hk

--- db/migrate/20191016131541_remove_enabled_from_clients.rb
+++
@@ -1,5 +0,0 @@
-class RemoveEnabledFromClients < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :clients, :enabled, :boolean
-  end
-end
--- db/migrate/20191016131624_add_enabled_to_clients.rb
+++
@@ -1,5 +0,0 @@
-class AddEnabledToClients < ActiveRecord::Migration[5.2]
-  def change
-    add_column :clients, :enabled, :boolean, null: false, default: true
-  end
-end
